085654196fe07744484502e70aaa9b08e19f0d93
bgarciaa181 <bgarcia181@ieszaidinvergeles.org>
2026-09-25 12:21:21 +0200

    Fix syntax and paths in LAMP config script

--- scripts/02_configure_lamp.sh
+++ scripts/02_configure_lamp.sh
@@ -2,7 +2,10 @@
 # scripts/02_configure_lamp.sh
 # Configure LAMP stack services and test page
 set -xeu
-chown -R www-data:www-data /var/www/html chmod -R 755 /var/www/html
-cp-vf /files/info.php/var/www/html/test.php
-systemctl enable-now apache2
-systemctl enable-now mariadb
+
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+cp -vf /vagrant/files/info.php /var/www/html/info.php
+
+systemctl enable --now apache2
+systemctl enable --now mariadb
